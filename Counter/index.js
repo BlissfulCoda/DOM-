1f7dabef4e0a546c5a2d545ae4f180ba7915ca11
Ronnie Kiyegga <ronniekiyegga@ronnies-mbp-2.home>
2022-05-09 15:27:25 +0100

    refactor js code using switch statement

--- Counter/index.js
+++ Counter/index.js
@@ -1,19 +1,39 @@
-const increaseBtn = document.querySelector(".increase");
-const decreaseBtn = document.querySelector(".decrease");
-const resetBtn = document.querySelector(".reset");
-const number = document.getElementById('number');
+const value = document.getElementById("number");
+const btns = document.querySelectorAll(".btn");
+// increaseBtn.addEventListener('click', function() {
+//    let increasedNumber = +(number.textContent);
+//    increasedNumber++
+//    return number.textContent = increasedNumber;
+// })
+// decreaseBtn.addEventListener('click', function() {
+//    let decreasedNumber = +(number.textContent);
+//    decreasedNumber--;
+//    return number.textContent = decreasedNumber;
+// })
 
-increaseBtn.addEventListener('click', function() {
-   let increasedNumber = +(number.textContent);
-   increasedNumber++
-   return number.textContent = increasedNumber;
-})
-decreaseBtn.addEventListener('click', function() {
-   let decreasedNumber = +(number.textContent);
-   decreasedNumber--;
-   return number.textContent = decreasedNumber;
-})
+// resetBtn.addEventListener('click', function() {
+//     number.textContent = 0;
+// })
 
-resetBtn.addEventListener('click', function() {
-    number.textContent = 0;
-})
+let count = 0;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let targetButton = e.currentTarget.className;
+
+    switch (targetButton) {
+      case "btn increase":
+        count++;
+        value.textContent = count;
+        break;
+      case "btn decrease":
+        count--;
+        value.textContent = count;
+        break;
+      case "btn reset":
+        value.textContent = 0;
+      default:
+        value.textContent = 0;
+    }
+  });
+});
